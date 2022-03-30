68ffd53fbfa05b27114311b79efcf8bcf559ce23
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-03-29 20:34:58 -0700

    fix: residual pred tensor sizes

--- deep_compression/layers/residual_pred.py
+++ deep_compression/layers/residual_pred.py
@@ -7,23 +7,25 @@ class ResidualPredBlock(nn.Module):
     def __init__(self, num_channels: int, pred_proportion: float = 0.5):
         super().__init__()
 
-        c_p = int(num_channels * pred_proportion)
-        self.c_p = c_p
+        c_x1 = int(num_channels * pred_proportion)
+        c_x2 = num_channels - c_x1
+        self.c_x1 = c_x1
+        self.c_x2 = c_x2
 
         self.pred_net = nn.Sequential(
-            nn.Conv2d(c_p, c_p, 1, stride=2, padding=0),
+            nn.Conv2d(c_x1, c_x1, 1, padding=0),
             nn.ReLU(inplace=True),
-            nn.Conv2d(c_p, c_p, 3, stride=2, padding=1),
+            nn.Conv2d(c_x1, c_x2, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(c_p, c_p, 3, stride=2, padding=1),
+            nn.Conv2d(c_x2, c_x2, 3, padding=1),
             nn.ReLU(inplace=True),
         )
 
     def forward(self, x: Tensor) -> Tensor:
         # Split into two tensors.
         # x1 is used to predict x2 (and is thus passed through).
-        x1 = x[:, : self.c_p]
-        x2 = x[:, self.c_p :]
+        x1 = x[:, : self.c_x1]
+        x2 = x[:, self.c_x1 :]
 
         y1 = x1  # pass-through
         y2 = x2 - self.pred_net(x1)  # residual
